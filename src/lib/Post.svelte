66c76fa46fd7ffd23986bc01d24a3c00d6eb1861
Marcin Borowski <mborowski@mindgroup.pl>
2023-04-18 21:11:19 +0200

    Initial commit

---
+++ src/lib/Post.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title = "Ala ma kota"
+</script>
+
+<div class="border border-stone-600 bg-stone-200 drop-shadow-md p-4">
+  <h2>{title}</h2>
+
+  <div class="text-sm">
+    <slot />
+  </div>
+</div>
---
+++ src/lib/PostForm.svelte
@@ -0,0 +1,34 @@
+<!-- @format -->
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let titleInput;
+  let title = "";
+  let abstract = "";
+
+  const dispatch = createEventDispatcher();
+
+  const onAddPost = () => {
+    if (title && abstract) {
+      dispatch("dodaj", {
+        title,
+        abstract,
+      });
+
+      title = "";
+      abstract = "";
+      titleInput.focus();
+    }
+  };
+
+  const onGetPosts = () => {
+    dispatch("pobierz");
+  };
+</script>
+
+<div class="bg-indigo-500 p-8 w-3/5 mx-auto my-8 flex flex-col gap-4">
+  <input bind:this={titleInput} bind:value={title} class="py-1 px-2" placeholder="Tytuł…" />
+  <textarea bind:value={abstract} class="py-1 px-2" placeholder="Zajawka…" />
+  <button on:click={onAddPost} class="bg-stone-500 text-white px-3 py-1 hover:bg-stone-700">Dodaj</button>
+  <button on:click={onGetPosts} class="bg-stone-500 text-white px-3 py-1 hover:bg-stone-700">Pobierz</button>
+</div>
